cfbf779f767685f8f098b1e14d5d5557be5fd072
iv77777 <70826113+iv77777@users.noreply.github.com>
2021-12-01 17:34:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,66 +14,73 @@ const After838 = document.querySelector('#after838');
 const cashBefore = document.querySelector('#cash-before');
 const cashAfter = document.querySelector('#cash-after');
 
-const beforeKm = document.querySelector('#before-km');
-const afterKm = document.querySelector('#after-km');
+const idKm = document.querySelector('#idKm');
 
 const privat = document.querySelector('#privat');
 
+const button = document.querySelector('#button');
+const buttonReverse = document.querySelector('#button-reverse');
 
-// находим кнопку рассчитать
-const button = document.querySelector('.button');
-
-// находим поле куда будем вывадить результат
 const earned = document.querySelector('#earned');
 
 
-// запускаем функцию miscalculation при клики по кнопке
+
+// Шлушаем клик по кнопке Рассчитать и при клики запускаем фуекцию miscalculation
 button.addEventListener('click', miscalculation);
 
+// Шлушаем клик по кнопке Реверс и при клики запускаем фуекцию reverseValue
+buttonReverse.addEventListener('click', reverseValue);
 
 
-// Функция которая делает прощот
+// Рсчитует заработок
 function miscalculation(){
 
-  // выщитуем разницу по службам и налички
-  const uklon = uklonAfter.value.replace(/\s/g, '') - uklonBefore.value.replace(/\s/g, '');
-  const bolt = boltAfter.value.replace(/\s/g, '') - boltBefore.value.replace(/\s/g, '');
-  const v838 = after838.value.replace(/\s/g, '') - before838.value.replace(/\s/g, '');
-  const cash = cashAfter.value.replace(/\s/g, '') - cashBefore.value.replace(/\s/g, '');
+  const uklon = uklonAfter.value - uklonBefore.value;
+  const bolt = boltAfter.value - boltBefore.value;
+  const v838 = after838.value - before838.value;
+  const cash = cashAfter.value - cashBefore.value;
 
   // -----------------------------------------------------------
-  // вищитуем сколько km проехал
-  let Km = afterKm.value.replace(/\s/g, '') - beforeKm.value.replace(/\s/g, '');
-
-  // Выщитуем сколько потратили на заправку газом
-  let priceKm = consumption.value.replace(/\s/g, '') * (Km / 100) * gasPrice.value.replace(/\s/g, '');
+   // витрата на газ
+  const priceKm = consumption.value * (idKm.value / 100) * gasPrice.value;
   // -----------------------------------------------------------
   
-  workValue = uklon + bolt + v838 + Number(privat.value.replace(/\s/g, '')) + cash - priceKm - WarmingUpTheCar.value.replace(/\s/g, '');
-
-  earned.innerHTML = Math.round(workValue);
+  // Высчитуетм виводит на страницу мой зароботок
+  earned.innerHTML = Math.round(uklon + bolt + v838 + Number(privat.value) + cash - priceKm - WarmingUpTheCar.value);
 }
 
-// ==========================================================
-// ==========================================================
-// ==========================================================
+// при фокусе на input убераем placeholder
 function validity(){
   // находим все импуты
   const inputs = document.querySelectorAll('input');
-
-
-// для каждого инпута
-inputs.forEach(function (input) {
-
-  // при фокусе на input убераем plasholder
+  // для каждого инпута
+  inputs.forEach(function (input) {
+  // при фокусе на input убераем placeholder
   input.onfocus = function () {
      input.placeholder = '';
     };
   });
 }
-
-
 validity();
 
+// миняет value в инпутах
+function reverseValue() {
+  uklonBefore.value = uklonAfter.value;
+  uklonAfter.value = '';
+  
+  boltBefore.value = boltAfter.value;
+  boltAfter.value = '';
 
-// ---------------------------------------------------
+  Before838.value = After838.value;
+  After838.value = '';
+
+  cashBefore.value = 400;
+  cashAfter.value = '';
+
+  idKm.value = '';
+
+  privat.value = '';
+
+  earned.innerHTML = '--';
+  
+}
